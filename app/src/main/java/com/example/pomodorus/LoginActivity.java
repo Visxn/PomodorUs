871d0a84c92517258247d202c0df617a20572229
visxn <rogervison@gmail.com>
2022-11-08 12:03:27 +0100

    Login and signup layouts working

---
+++ app/src/main/java/com/example/pomodorus/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.pomodorus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton fabLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.vpMain);
+        fabLogin = findViewById(R.id.fab_login);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+        tabLayout.setTabGravity(tabLayout.GRAVITY_FILL);
+
+
+    }
+}
